2c525d42754eb9e3b0c76a9ab8292c3d062119d6
unknown <paranthamanganapathy@gmail.com>
2020-09-30 01:36:53 +0530

    Event communication

---
+++ force-app/main/default/lwc/examplea20EventChild/examplea20EventChild.js
@@ -0,0 +1,14 @@
+import { LightningElement,api } from 'lwc';
+
+export default class Examplea20EventChild extends LightningElement {
+
+    @api meetingRoomInfo = {roomName:'A-01', roomCapacity:'12'}
+    @api showRoomInfo = false;
+
+    tileClickHandler(){
+        const tileClicked = new CustomEvent('tileclick', {detail : this.meetingRoomInfo, bubbles :true});
+        this.dispatchEvent(tileClicked);
+    }
+
+
+}
