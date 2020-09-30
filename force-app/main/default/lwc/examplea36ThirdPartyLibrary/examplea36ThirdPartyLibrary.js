1195ae63d75b93bda1196ae973b25b6d1d5a2b00
unknown <paranthamanganapathy@gmail.com>
2020-10-01 01:57:37 +0530

    Thirdparty library

---
+++ force-app/main/default/lwc/examplea36ThirdPartyLibrary/examplea36ThirdPartyLibrary.js
@@ -0,0 +1,27 @@
+import { LightningElement } from 'lwc';
+import SL from '@salesforce/resourceUrl/simplelightbox';
+import { loadStyle, loadScript } from 'lightning/platformResourceLoader';
+
+export default class Examplea36ThirdPartyLibrary extends LightningElement {
+
+    slLoaded = false;
+    
+    renderedCallback(){
+        if(!this.slLoaded){
+            Promise.all([
+                loadStyle(this, SL+'/simpleLightbox/dist/simpleLightbox.css'),
+                loadScript(this, SL+'/simpleLightbox/dist/simpleLightbox.js')
+            ]).then(()=>{
+                this.slLoaded = true;
+            }).catch((error)=>{
+                console.error('Could not initilize simple light box-', error);
+            });
+        }
+    }
+
+    openGallery(){
+        SimpleLightbox.open({
+            items: ['/resource/cars/van/maruti_suzuki_eeco.jpg', '/resource/cars/luxury/mercedes_benz_gls.jpg', '/resource/cars/sports/Audi_R8_V10_Plus.jpg']
+        });
+    }
+}
