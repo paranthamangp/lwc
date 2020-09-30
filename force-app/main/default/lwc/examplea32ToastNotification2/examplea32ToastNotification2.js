af67d5d0ce81301b4e8a6df00119a9f59a1d5f43
unknown <paranthamanganapathy@gmail.com>
2020-10-01 00:43:19 +0530

    Toast notification example 2

---
+++ force-app/main/default/lwc/examplea32ToastNotification2/examplea32ToastNotification2.js
@@ -0,0 +1,41 @@
+import { LightningElement, track } from 'lwc';
+import getAllAccounts from '@salesforce/apex/AccountManagerNonCacheable.getAccount'
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+
+
+export default class Examplea32ToastNotification2 extends LightningElement {
+
+    numberOfRecords;
+    accounts;
+
+    get responseReceived(){
+        if(this.accounts){
+            return true;
+        }
+        return false;
+    }
+    numberOfAccountChangeHandler(event){
+        this.numberOfRecords = event.target.value;
+    }
+
+    getAccounts(){
+        getAllAccounts({numberOfRecords:this.numberOfRecords}).then(response =>{
+            this.accounts = response;
+            const toastEvent = new ShowToastEvent({
+                title : 'Accounts Loaded',
+                message : this.numberOfRecords + ' Accounts Fetched From Server',
+                variant : 'success',
+            });
+            this.dispatchEvent(toastEvent);
+        }).catch(error =>{
+            console.error('Error in getting the accounts', error.body.message);
+            const toastEvent = new ShowToastEvent({
+                title : 'ERROR',
+                message : error.body.message,
+                variant : 'error',
+            });
+            this.dispatchEvent(toastEvent);
+        })
+    }
+
+}
